237460d3ae8812752dbf928d5c98a875f8f8a84e
Jon Campbell <jonathan.campbell@firsthelpline.com>
2017-11-01 11:58:06 +0000

    Extract variables

--- ProNet/ProNet.Test/ProgrammerRankCalculatorTests.cs
+++ ProNet/ProNet.Test/ProgrammerRankCalculatorTests.cs
@@ -10,31 +10,42 @@ namespace ProNet.Test
         [Test]
         public void Programmer_with_no_recommendations()
         {
-            var programmer = new Programmer("a", new List<string>());
+            const string programmerId = "a";
+            const int settleLimit = 40;
+            const double expected = 0.15d;
+            const double range = 0.01d;
+
+            var programmer = new Programmer(programmerId, new List<string>());
             var programmerRepository = Substitute.For<IProgrammerRepository>();
-            programmerRepository.GetById("a").Returns(programmer);
+            programmerRepository.GetById(programmerId).Returns(programmer);
             programmerRepository.GetAll().Returns(new List<Programmer> {programmer});
             var rankCalculator = new ProgrammerRankCalculator(programmerRepository);
 
-            var rank = rankCalculator.GetRank("a", 40);
+            var rank = rankCalculator.GetRank(programmerId, settleLimit);
 
-            Assert.That(rank, Is.EqualTo(0.15d).Within(0.01d));
+            Assert.That(rank, Is.EqualTo(expected).Within(range));
         }
 
         [Test]
         public void Two_programmers_recommending_each_other()
         {
-            var programmerA = new Programmer("a", new List<string> { "b" });
-            var programmerB = new Programmer("b", new List<string> { "a" });
+            const string idA = "a";
+            const string idB = "b";
+            const int settleLimit = 40;
+            const double expected = 1.0d;
+            const double range = 0.01d;
+
+            var programmerA = new Programmer(idA, new List<string> { idB });
+            var programmerB = new Programmer(idB, new List<string> { idA });
             var programmerRepository = Substitute.For<IProgrammerRepository>();
-            programmerRepository.GetById("a").Returns(programmerA);
-            programmerRepository.GetById("b").Returns(programmerB);
+            programmerRepository.GetById(idA).Returns(programmerA);
+            programmerRepository.GetById(idB).Returns(programmerB);
             programmerRepository.GetAll().Returns(new List<Programmer> { programmerA, programmerB });
             var rankCalculator = new ProgrammerRankCalculator(programmerRepository);
 
-            var rank = rankCalculator.GetRank("a", 40);
+            var rank = rankCalculator.GetRank(idA, settleLimit);
 
-            Assert.That(rank, Is.EqualTo(1.0d).Within(0.01d));
+            Assert.That(rank, Is.EqualTo(expected).Within(range));
         }
     }
 }
