15771b59abfdfca702babf97889ddc010f3f36b2
Jon Campbell <jonathan.campbell@firsthelpline.com>
2017-11-08 16:06:33 +0000

    Combine tests to paramaterised TestCases

--- ProNet/ProNet.Test/SeparationServiceTests.cs
+++ ProNet/ProNet.Test/SeparationServiceTests.cs
@@ -6,29 +6,19 @@ namespace ProNet.Test
     [TestFixture]
     public class SeparationServiceTests
     {
-        [Test]
-        public void Programmers_with_no_connection()
+        [TestCase("a", "b", -1)]
+        [TestCase("a", "a", 0)]
+        public void Programmers_with_no_connection(string programmerAId, string programmerBId, int expected)
         {
-            const int expected = -1;
             var programmerRepository = Substitute.For<IProgrammerRepository>();
-            var programmerA = new Programmer("a", null, null);
-            programmerRepository.GetById("a").Returns(programmerA);
-            var programmerB = new Programmer("b", null, null);
-            programmerRepository.GetById("b").Returns(programmerB);
-            var separationService = new SeparationService(programmerRepository);
-            var degrees = separationService.GetDegreesOfSeparation("a", "b");
-            Assert.That(degrees, Is.EqualTo(expected));
-        }
 
-        [Test]
-        public void Separation_with_self()
-        {
-            const int expected = 0;
-            var programmerRepository = Substitute.For<IProgrammerRepository>();
-            var programmer = new Programmer("a", null, null);
-            programmerRepository.GetById("a").Returns(programmer);
-            var separationService = new SeparationService(programmerRepository);
-            var degrees = separationService.GetDegreesOfSeparation("a", "a");
+            var programmerA = new Programmer(programmerAId, null, null);
+            programmerRepository.GetById(programmerAId).Returns(programmerA);
+
+            var programmerB = new Programmer(programmerBId, null, null);
+            programmerRepository.GetById(programmerBId).Returns(programmerB);
+
+            var degrees = new SeparationService(programmerRepository).GetDegreesOfSeparation(programmerAId, programmerBId);
             Assert.That(degrees, Is.EqualTo(expected));
         }
     }
