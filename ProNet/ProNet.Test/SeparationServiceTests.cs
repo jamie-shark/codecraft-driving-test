d81046d7c92a4fcb0995cee1dab7b7108c03c030
Jon Campbell <jonathan.campbell@firsthelpline.com>
2017-11-08 16:50:10 +0000

    Extract method

--- ProNet/ProNet.Test/SeparationServiceTests.cs
+++ ProNet/ProNet.Test/SeparationServiceTests.cs
@@ -10,33 +10,37 @@ namespace ProNet.Test
         [TestCase("a", "a", 0)]
         public void Programmers_with_no_connection(string programmerAId, string programmerBId, int expected)
         {
-            var programmerRepository = Substitute.For<IProgrammerRepository>();
+            var programmerRepository = StubProgrammerRepository(
+                new Programmer(programmerAId, new string[] { }, null),
+                new Programmer(programmerBId, new string[] { }, null));
 
-            var programmerA = new Programmer(programmerAId, new string[] {}, null);
-            programmerRepository.GetById(programmerAId).Returns(programmerA);
-
-            var programmerB = new Programmer(programmerBId, new string[] { }, null);
-            programmerRepository.GetById(programmerBId).Returns(programmerB);
-
-            var degrees = new SeparationService(programmerRepository).GetDegreesOfSeparation(programmerAId, programmerBId);
-            Assert.That(degrees, Is.EqualTo(expected));
+            AssertDegreesOfSeparation(programmerRepository, programmerAId, programmerBId, expected);
         }
 
         [Test]
         public void Programmers_with_recommendations_are_1_degree_apart()
         {
             const int expected = 1;
-            var programmerRepository = Substitute.For<IProgrammerRepository>();
-
             const string programmerAId = "a";
             const string programmerBId = "b";
 
-            var programmerA = new Programmer(programmerAId, new [] {programmerBId}, null);
-            programmerRepository.GetById(programmerAId).Returns(programmerA);
+            var programmerRepository = StubProgrammerRepository(
+                new Programmer(programmerAId, new[] {programmerBId}, null),
+                new Programmer(programmerBId, new string[] { }, null));
 
-            var programmerB = new Programmer(programmerBId, new string[] { }, null);
-            programmerRepository.GetById(programmerBId).Returns(programmerB);
+            AssertDegreesOfSeparation(programmerRepository, programmerAId, programmerBId, expected);
+        }
 
+        private static IProgrammerRepository StubProgrammerRepository(IProgrammer programmerA, IProgrammer programmerB)
+        {
+            var programmerRepository = Substitute.For<IProgrammerRepository>();
+            programmerRepository.GetById(programmerA.GetId()).Returns(programmerA);
+            programmerRepository.GetById(programmerB.GetId()).Returns(programmerB);
+            return programmerRepository;
+        }
+
+        private static void AssertDegreesOfSeparation(IProgrammerRepository programmerRepository, string programmerAId, string programmerBId, int expected)
+        {
             var degrees = new SeparationService(programmerRepository).GetDegreesOfSeparation(programmerAId, programmerBId);
             Assert.That(degrees, Is.EqualTo(expected));
         }
