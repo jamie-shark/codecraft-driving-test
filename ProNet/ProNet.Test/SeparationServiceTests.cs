6b18b2ac6ec7ff1b986df4a2ad04054bc394ff5e
Jon Campbell <jonathan.campbell@firsthelpline.com>
2017-11-08 16:01:07 +0000

    Add SeparationService

---
+++ ProNet/ProNet.Test/SeparationServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace ProNet.Test
+{
+    [TestFixture]
+    public class SeparationServiceTests
+    {
+        [Test]
+        public void Programmers_with_no_connection()
+        {
+            const int expected = -1;
+            var programmerRepository = Substitute.For<IProgrammerRepository>();
+            var programmerA = new Programmer("a", null, null);
+            programmerRepository.GetById("a").Returns(programmerA);
+            var programmerB = new Programmer("b", null, null);
+            programmerRepository.GetById("b").Returns(programmerB);
+            var separationService = new SeparationService(programmerRepository);
+            var degrees = separationService.GetDegreesOfSeparation("a", "b");
+            Assert.That(degrees, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ ProNet/ProNet/SeparationService.cs
@@ -0,0 +1,17 @@
+﻿namespace ProNet
+{
+    public class SeparationService
+    {
+        private readonly IProgrammerRepository _programmerRepository;
+
+        public SeparationService(IProgrammerRepository programmerRepository)
+        {
+            _programmerRepository = programmerRepository;
+        }
+
+        public int GetDegreesOfSeparation(string programmerA, string programmerB)
+        {
+            return -1;
+        }
+    }
+}
