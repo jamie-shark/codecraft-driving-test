2f7a3a936932c2b0e2a54f50dc8ae4ac686a6846
Jon Campbell <jonathan.campbell@firsthelpline.com>
2017-11-29 18:47:12 +0000

    Make TeamStrengthService return 0 for teams with no relationships

--- ProNet/ProNet.Test/TeamStrengthServiceTests.cs
+++ ProNet/ProNet.Test/TeamStrengthServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace ProNet.Test
@@ -6,12 +7,22 @@ namespace ProNet.Test
     [TestFixture]
     public class TeamStrengthServiceTests
     {
-        [TestCase("", new string[] {}, 0)]
-        [TestCase("", new[] { "leader" }, 0)]
-        public void Strength_of_empty_team_is_zero(string language, IEnumerable<string> team, decimal expected)
+        [TestCase(new string[] {}, 0)]
+        [TestCase(new[] { "leader" }, 0)]
+        public void Strength_of_empty_team_is_zero(IEnumerable<string> team, decimal expected)
         {
-            var strength = new TeamStrengthService().GetStrength(language, team);
+            var strength = new TeamStrengthService(null).GetStrength("", team);
             Assert.That(strength, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Strength_of_team_with_no_connection_is_zero()
+        {
+            var team = new List<string> {"leader", "a", "b"};
+            var separationService = Substitute.For<ISeparationService>();
+            separationService.GetDegreesBetween(Arg.Any<string>(), Arg.Any<string>()).Returns(0);
+            var strength = new TeamStrengthService(separationService).GetStrength("", team);
+            Assert.That(strength, Is.EqualTo(0));
         }
     }
 }
--- ProNet/ProNet/TeamStrengthService.cs
+++ ProNet/ProNet/TeamStrengthService.cs
@@ -4,6 +4,13 @@ namespace ProNet
 {
     public class TeamStrengthService
     {
+        private readonly ISeparationService _separationService;
+
+        public TeamStrengthService(ISeparationService separationService)
+        {
+            _separationService = separationService;
+        }
+
         public double GetStrength(string language, IEnumerable<string> team)
         {
             return 0;
