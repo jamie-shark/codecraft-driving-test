fdbe223cc012b4024ef7bf427e15d68d712e6ca0
Jon Campbell <jonathan.campbell@firsthelpline.com>
2017-11-29 18:55:51 +0000

    Verify TeamStrengthService returns 0 when team member does not have the skill

--- ProNet/ProNet.Test/TeamStrengthServiceTests.cs
+++ ProNet/ProNet.Test/TeamStrengthServiceTests.cs
@@ -11,7 +11,7 @@ namespace ProNet.Test
         [TestCase(new[] { "leader" }, 0)]
         public void Strength_of_empty_team_is_zero(IEnumerable<string> team, decimal expected)
         {
-            var strength = new TeamStrengthService(null).GetStrength("", team);
+            var strength = new TeamStrengthService(null, null).GetStrength("", team);
             Assert.That(strength, Is.EqualTo(expected));
         }
 
@@ -21,7 +21,19 @@ namespace ProNet.Test
             var team = new List<string> {"leader", "a", "b"};
             var separationService = Substitute.For<ISeparationService>();
             separationService.GetDegreesBetween(Arg.Any<string>(), Arg.Any<string>()).Returns(0);
-            var strength = new TeamStrengthService(separationService).GetStrength("", team);
+            var strength = new TeamStrengthService(separationService, null).GetStrength("", team);
+            Assert.That(strength, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Strength_of_team_with_member_that_does_not_know_chosen_skill_is_zero()
+        {
+            var team = new List<string> { "leader", "a", "b" };
+            var skillService = Substitute.For<ISkillsService>();
+            skillService.GetSkills("leader").Returns(new [] { "skill" });
+            skillService.GetSkills("a").Returns(new[] { "NOT skill" });
+            skillService.GetSkills("b").Returns(new[] { "skill" });
+            var strength = new TeamStrengthService(null, skillService).GetStrength("skill", team);
             Assert.That(strength, Is.EqualTo(0));
         }
     }
--- ProNet/ProNet/TeamStrengthService.cs
+++ ProNet/ProNet/TeamStrengthService.cs
@@ -5,10 +5,12 @@ namespace ProNet
     public class TeamStrengthService
     {
         private readonly ISeparationService _separationService;
+        private readonly ISkillsService _skillService;
 
-        public TeamStrengthService(ISeparationService separationService)
+        public TeamStrengthService(ISeparationService separationService, ISkillsService skillService)
         {
             _separationService = separationService;
+            _skillService = skillService;
         }
 
         public double GetStrength(string language, IEnumerable<string> team)
