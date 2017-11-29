df0375e9ee17ee5b562b3d69f9a398cff0bfbeda
Jon Campbell <jonathan.campbell@firsthelpline.com>
2017-11-29 18:36:54 +0000

    Make TeamStrengthService

---
+++ ProNet/ProNet.Test/TeamStrengthServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ProNet.Test
+{
+    [TestFixture]
+    public class TeamStrengthServiceTests
+    {
+        [Test]
+        public void Strength_of_empty_team_is_zero()
+        {
+            var language = "";
+            var team = new string[] {};
+            var strength = new TeamStrengthService().GetStrength(language, team);
+            Assert.That(strength, Is.EqualTo(0));
+        }
+    }
+}
---
+++ ProNet/ProNet/TeamStrengthService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ProNet
+{
+    public class TeamStrengthService
+    {
+        public double GetStrength(string language, IEnumerable<string> team)
+        {
+            return 0;
+        }
+    }
+}
