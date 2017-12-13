79f42944043523dc9b69886cc4600fd70868864e
Jon Campbell <jonathan.campbell@firsthelpline.com>
2017-12-13 15:59:33 +0000

    Add failing team strength test

--- ProNet/ProNet.Test/StrongestTeamServiceTests.cs
+++ ProNet/ProNet.Test/StrongestTeamServiceTests.cs
@@ -27,6 +27,32 @@ namespace ProNet.Test
         }
 
         [Test]
+        public void Strongest_team_has_the_highest_possible_team_strength_for_given_skill_and_size()
+        {
+            const string skill = "valid skill";
+            const string programmerA = "a";
+            const string programmerB = "b";
+            const string programmerC = "c";
+
+            var networkRepository = Substitute.For<INetworkRepository>();
+            networkRepository.GetAll().Returns(new[]
+            {
+                new Programmer(programmerA, null, null),
+                new Programmer(programmerB, null, null),
+                new Programmer(programmerC, null, null)
+            });
+
+            var teamService = Substitute.For<ITeamStrengthService>();
+            teamService.GetStrength(skill, new[] { programmerA, programmerB }).Returns(0d);
+            teamService.GetStrength(skill, new[] { programmerC, programmerB }).Returns(1d);
+            teamService.GetStrength(skill, new[] { programmerA, programmerC }).Returns(2d);
+
+            var strongestTeam = new StrongestTeamService(networkRepository, teamService).FindStrongestTeam(skill, 2);
+
+            Assert.That(strongestTeam, Is.EqualTo(new[] { programmerA, programmerC }));
+        }
+
+        [Test]
         public void Strongest_team_must_have_at_least_one_member()
         {
             Assert.Throws<ArgumentException>(() => new StrongestTeamService(Substitute.For<INetworkRepository>(), Substitute.For<ITeamStrengthService>()).FindStrongestTeam("valid skill", 0));
