b8563b0ebb87e9e555caa95e9b727fa36927a7e1
Jon Campbell <jonathan.campbell@firsthelpline.com>
2017-12-20 18:34:59 +0000

    Add CombinationService, another implementation of IPermutationService that also gives ordered results

---
+++ ProNet/ProNet.Test/Util/CombinationServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using ProNet.Util;
+
+namespace ProNet.Test.Util
+{
+    [TestFixture]
+    public class CombinationServiceTests
+    {
+        [TestCase(1, 5)]
+        [TestCase(2, 20)]
+        [TestCase(3, 60)]
+        [TestCase(4, 120)]
+        [TestCase(5, 120)]
+        public void Correct_number_of_results(int size, int expected)
+        {
+            var set = new[] { 1, 2, 3, 4, 5 };
+            var combinations = new CombinationService().GetPermutations(set, size);
+            Assert.That(combinations.Count(), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void No_duplicates_in_any_combination()
+        {
+            var set = new[] { 1, 2, 3, 4, 5 };
+            var combinations = new CombinationService().GetPermutations(set, 3);
+            var duplicateCombinations = combinations.Where(combination => combination.Distinct().Count() != combination.Count());
+            Assert.That(duplicateCombinations.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Different_orderings_of_each_permutation_are_returned()
+        {
+            var set = new[] { 1, 2, 3, 4, 5 };
+            var combinations = new CombinationService().GetPermutations(set, 3);
+            var combinationsWithOneTwoAndThree = combinations.Where(combination => combination.Contains(1) && combination.Contains(2) && combination.Contains(3));
+            Assert.That(combinationsWithOneTwoAndThree.Count(), Is.EqualTo(6));
+        }
+    }
+}
---
+++ ProNet/ProNet/Util/CombinationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProNet.Util
+{
+    public class CombinationService : IPermutationService
+    {
+        public IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> set, int size) where T : IEquatable<T>
+        {
+            if (size == 1)
+                return set.Select(item => new[] {item});
+
+            return GetPermutations(set, size - 1)
+                .SelectMany( item               => set.Where(i => !item.Contains(i)),
+                            (item, combination) => item.Concat(new[] { combination }.AsEnumerable()));
+        }
+    }
+}
